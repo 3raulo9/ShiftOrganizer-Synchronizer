8f60da96f4d9fb4cfa460f76f7a38a70f299b4e6
Raul <123974722+3raulo9@users.noreply.github.com>
2024-07-07 10:46:54 +0300

    added a loader to the home page

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import UserForm from '../components/UserForm';
 import UserCard from '../components/UserCard';
 import '../styles/Home.css';
 
 const Home = ({ handleSubmit, userData, numUsers, setNumUsers }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="home">
-      <UserForm numUsers={numUsers} setNumUsers={setNumUsers} handleSubmit={handleSubmit} />
-      <div className="user-cards">
-        {userData.map((user, index) => (
-          <UserCard key={index} user={user} />
-        ))}
-      </div>
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="content">
+          <UserForm numUsers={numUsers} setNumUsers={setNumUsers} handleSubmit={handleSubmit} />
+          <div className="user-cards">
+            {userData.map((user, index) => (
+              <UserCard key={index} user={user} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
