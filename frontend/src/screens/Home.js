1d0ae3827d8b7d65672379243d61f76c6e907bf5
Raul <123974722+3raulo9@users.noreply.github.com>
2024-07-15 20:35:49 +0300

    Update Home.js

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -27,7 +27,7 @@ const Home = ({ handleSubmit, handleTest, userData, numUsers, setNumUsers, setLo
           setLoading={setLoading} 
           handleTest={handleTest}
         />
-        {userData.length > 0 && (
+        {userData.length > 0 && !isDrawerOpen && (
           <IconButton 
             onClick={toggleDrawer(true)} 
             className="drawer-toggle-button"
@@ -36,11 +36,20 @@ const Home = ({ handleSubmit, handleTest, userData, numUsers, setNumUsers, setLo
             <ArrowForwardIosIcon fontSize="large" />
           </IconButton>
         )}
+        {isDrawerOpen && (
+          <IconButton 
+            onClick={toggleDrawer(false)} 
+            className="drawer-toggle-button"
+            style={{ position: 'fixed', right: 0, top: '50%', transform: 'translateY(-50%)', zIndex: 1000 }}
+          >
+            <CloseIcon fontSize="large" />
+          </IconButton>
+        )}
         <Drawer
           anchor="right"
           open={isDrawerOpen}
           onClose={toggleDrawer(false)}
-          PaperProps={{ style: { width: '900px' } }} // Increase the width to twice the original size
+          PaperProps={{ style: { width: '1300px' } }} // Set the width to desired size
         >
           <div
             role="presentation"
@@ -48,9 +57,6 @@ const Home = ({ handleSubmit, handleTest, userData, numUsers, setNumUsers, setLo
             onKeyDown={toggleDrawer(false)}
             style={{ padding: 16 }}
           >
-            <IconButton onClick={toggleDrawer(false)} style={{ marginBottom: 16 }}>
-              <CloseIcon />
-            </IconButton>
             <div className="user-cards">
               {userData.map((user, index) => (
                 <UserCard key={index} user={user} />
